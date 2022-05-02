28aa684a209dfb45cda2c9b391812a17958a5c94
Zhou-jw <77321888+Zhou-jw@users.noreply.github.com>
2022-05-02 23:28:49 +0800

    Update dgcnn_cls.py

--- perception3d/models/classification/object/dgcnn_cls.py
+++ perception3d/models/classification/object/dgcnn_cls.py
@@ -18,14 +18,3 @@ class DGCNNCls(nn.Module):
         label_logit = self.encoder(**inputs)
         return {'pred_label_logit': label_logit}
 
-    """
-    question:
-    1. 什么是 embedding
-    2. knn 算法实现
-    3. get_graph_feature细节(done)
-    4. 点云里的in_channels
-    """
-
-    """
-    配置文件不能写多余的参数
-    """
